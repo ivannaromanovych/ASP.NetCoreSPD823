ecf2b0fc00cb22f9cdcc46b03639ae3fc6f766cc
Мельничук <melnichukmyk@rivne.itstep.org>
2020-03-14 15:43:46 +0200

    ShowListConsumer and addet

---
+++ Komunalka/Controllers/ConsumerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Komunalka.Entities;
+using Komunalka.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Komunalka.Controllers
+{
+    public class ConsumerController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        public ConsumerController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            List<ConsumerViewModel> model = context.Consumers.Select(c => new ConsumerViewModel
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Address = c.Address
+            }).ToList();
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+       [HttpPost]
+        public IActionResult Create(ConsumerCreateViewModel model)
+        {
+            Consumer consumer = new Consumer
+            {
+                Name = model.Name,
+                Address = model.Address
+            };
+            context.Consumers.Add(consumer);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Komunalka/Models/ConsumerViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komunalka.Models
+{
+    public class ConsumerViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="Постачальник")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name="Адреса")]
+        public string Address { get; set; }
+    }
+    public class ConsumerCreateViewModel
+    {
+        [Required]
+        [Display(Name = "Постачальник")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Адреса")]
+        public string Address { get; set; }
+    }
+}
