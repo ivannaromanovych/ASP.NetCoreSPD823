f60032eeef458f0a26df7d2fa20755b746269fe6
Іванна Романович <ivankaromanovych12@gmail.com>
2020-03-14 15:03:44 +0200

    add database sqlite

---
+++ Komunalka/Entities/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komunalka.Entities
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Consumer> Consumers { get; set; }
+    }
+}
---
+++ Komunalka/Entities/Consumer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komunalka.Entities
+{
+    [Table("tblConsumers")]
+    public class Consumer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(200)]
+        public string Name { get; set; }
+        [Required, StringLength(1000)]
+        public string Address { get; set; }
+    }
+}
